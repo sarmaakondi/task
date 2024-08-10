9eeedf9007d35c28fd0fc299ad672dcac5e908f2
avnms <avnmsarma@gmail.com>
2024-08-10 12:54:34 +1000

    Move core functionality to taskController and import them in taskRoutes and use

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -3,4 +3,19 @@ const getTasks = (req, res) => {
     res.status(200).json({ message: "Get all tasks" });
 };
 
-module.exports = { getTasks };
+// Create task
+const setTask = (req, res) => {
+    res.status(200).json({ message: "Task created." });
+};
+
+// Update task
+const updateTask = (req, res) => {
+    res.status(200).json({ message: `Task ${req.params.id} updated.` });
+};
+
+// Delete task
+const deleteTask = (req, res) => {
+    res.status(200).json({ message: `Task ${req.params.id} deleted.` });
+};
+
+module.exports = { getTasks, setTask, updateTask, deleteTask };
--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -1,23 +1,16 @@
 const express = require("express");
 const router = express.Router();
-const { getTasks } = require("../controllers/taskController");
+const {
+    getTasks,
+    setTask,
+    updateTask,
+    deleteTask,
+} = require("../controllers/taskController");
 
-// Get all tasks
+// routes
 router.get("/", getTasks);
-
-// Create task
-router.post("/", (req, res) => {
-    res.status(200).json({ message: "Task created." });
-});
-
-// Update task
-router.put("/:id", (req, res) => {
-    res.status(200).json({ message: `Task ${req.params.id} updated.` });
-});
-
-// Delete task
-router.delete("/:id", (req, res) => {
-    res.status(200).json({ message: `Task ${req.params.id} deleted.` });
-});
+router.post("/", setTask);
+router.put("/:id", updateTask);
+router.delete("/:id", deleteTask);
 
 module.exports = router;
