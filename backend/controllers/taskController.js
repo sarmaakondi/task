3f752c8b7afff45f1774ae868fc98165d74dd74f
avnms <avnmsarma@gmail.com>
2024-08-10 15:24:11 +1000

    Refactor code to improvie efficiency

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -10,12 +10,13 @@ const getTasks = asyncHandler(async (req, res) => {
 
 // Create task
 const setTask = asyncHandler(async (req, res) => {
-    if (!req.body.text) {
+    const { text } = req.body;
+    if (!text) {
         res.status(400);
         throw new Error("Task cannot be empty!");
     }
 
-    const task = await Task.create({ text: req.body.text });
+    const task = await Task.create({ text });
     res.status(200).json(task);
 });
 
@@ -23,19 +24,18 @@ const setTask = asyncHandler(async (req, res) => {
 const updateTask = asyncHandler(async (req, res) => {
     const { id } = req.params;
     if (!mongoose.Types.ObjectId.isValid(id)) {
-        res.status(400);
+        res.status(404);
         throw new Error("Task not found");
     }
 
-    const task = await Task.findById(req.params.id);
-    if (!task) {
-        res.status(400);
-        throw new Error("Task not found");
-    }
-
-    const updatedTask = await Task.findByIdAndUpdate(req.params.id, req.body, {
+    const updatedTask = await Task.findByIdAndUpdate(id, req.body, {
         new: true,
     });
+    if (!updatedTask) {
+        res.status(404);
+        throw new Error("Task not found");
+    }
+
     res.status(200).json(updatedTask);
 });
 
@@ -43,18 +43,17 @@ const updateTask = asyncHandler(async (req, res) => {
 const deleteTask = asyncHandler(async (req, res) => {
     const { id } = req.params;
     if (!mongoose.Types.ObjectId.isValid(id)) {
-        res.status(400);
+        res.status(404);
         throw new Error("Task not found");
     }
 
-    const task = await Task.findById(req.params.id);
-    if (!task) {
-        res.status(400);
+    const deletedTask = await Task.findByIdAndDelete(id);
+    if (!deletedTask) {
+        res.status(404);
         throw new Error("Task not found");
     }
 
-    await Task.findByIdAndDelete(req.params.id);
-    res.status(200).json({ id: req.params.id });
+    res.status(200).json({ id });
 });
 
 module.exports = { getTasks, setTask, updateTask, deleteTask };
