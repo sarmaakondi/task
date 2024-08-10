7b4877fedb13e6c2e101ee7cb4b1ff8641271899
avnms <avnmsarma@gmail.com>
2024-08-10 16:58:46 +1000

    Update userRoutes to include routes functionality from userController

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,4 +1,14 @@
 const express = require("express");
 const router = express.Router();
+const {
+    registerUser,
+    loginUser,
+    getCurrentUser,
+} = require("../controllers/userController");
+
+// Routes
+router.post("/", registerUser);
+router.post("/login", loginUser);
+router.get("/current", getCurrentUser);
 
 module.exports = router;
