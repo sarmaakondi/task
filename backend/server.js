2d0ff4498af9560caa3a871b921d6867cd00779f
avnms <avnmsarma@gmail.com>
2024-08-10 13:49:12 +1000

    Create errorHandler middleware and use in server

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const dotenv = require("dotenv").config();
 const port = process.env.PORT || 5000;
 
+const { errorHandler } = require("./middleware/errorMidleware");
+
 const app = express();
 
 app.use(express.json());
@@ -9,6 +11,9 @@ app.use(express.urlencoded({ extended: false }));
 
 // Routes
 app.use("/api/tasks", require("./routes/taskRoutes"));
+
+// Middleware
+app.use(errorHandler);
 
 app.listen(port, () =>
     console.log(`Server listening on http://localhost:${port}`)
