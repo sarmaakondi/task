1558006147a5e93d0a451df4bb51e25254314d6d
avnms <avnmsarma@gmail.com>
2024-08-10 18:56:00 +1000

    Check the task owner before proceeding with update

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -1,6 +1,7 @@
 const asyncHandler = require("express-async-handler");
 const Task = require("../models/taskModel");
 const mongoose = require("mongoose");
+const User = require("../models/userModel");
 
 // Get all tasks
 const getTasks = asyncHandler(async (req, res) => {
@@ -13,11 +14,11 @@ const setTask = asyncHandler(async (req, res) => {
     const { text } = req.body;
     if (!text) {
         res.status(400);
-        throw new Error("Task cannot be empty!");
+        throw new Error("Task text cannot be empty!");
     }
 
     const task = await Task.create({ text, user: req.user.id });
-    res.status(200).json(task);
+    res.status(201).json(task);
 });
 
 // Update task
@@ -26,6 +27,17 @@ const updateTask = asyncHandler(async (req, res) => {
     if (!mongoose.Types.ObjectId.isValid(id)) {
         res.status(404);
         throw new Error("Task not found");
+    }
+
+    const task = await Task.findById(id);
+    if (!task) {
+        res.status(404);
+        throw new Error("Task not found");
+    }
+
+    if (task.user.toString() !== req.user.id) {
+        res.status(403);
+        throw new Error("User is not authorized to update this task");
     }
 
     const updatedTask = await Task.findByIdAndUpdate(id, req.body, {
