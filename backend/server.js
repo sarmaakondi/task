69a38cf31782114152fece0af2c1d77c7882d0d1
avnms <avnmsarma@gmail.com>
2024-08-10 12:16:53 +1000

    Create sample get route for validation

--- backend/server.js
+++ backend/server.js
@@ -4,4 +4,11 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 
-app.listen(port, () => console.log(`Server listening on ${port}`));
+// Routes
+app.get("/api/tasks", (req, res) => {
+    res.send("Get all tasks");
+});
+
+app.listen(port, () =>
+    console.log(`Server listening on http://localhost:${port}`)
+);
