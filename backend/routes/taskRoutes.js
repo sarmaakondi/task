243ab7a849e7f151a26460b4cda70bd1b0b10020
avnms <avnmsarma@gmail.com>
2024-08-10 12:40:05 +1000

    Create placeholder routes for create, update, and delete task

--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -1,9 +1,24 @@
 const express = require("express");
 const router = express.Router();
 
-// Index route
+// Get all tasks
 router.get("/", (req, res) => {
     res.status(200).json({ message: "Get all tasks" });
+});
+
+// Create task
+router.post("/", (req, res) => {
+    res.status(200).json({ message: "Task created." });
+});
+
+// Update task
+router.put("/:id", (req, res) => {
+    res.status(200).json({ message: `Task ${req.params.id} updated.` });
+});
+
+// Delete task
+router.delete("/:id", (req, res) => {
+    res.status(200).json({ message: `Task ${req.params.id} deleted.` });
 });
 
 module.exports = router;
